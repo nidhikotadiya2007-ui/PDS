426e2657f589a2a972aef4ed97803f1aa06467bb
nidhikotadiya2007-ui <nidhikotadiya2007@gmail.com>
2026-09-19 12:14:12 +0530

    Create functions.py

---
+++ Simplilearn_PDS_096/functions.py
@@ -0,0 +1,25 @@
+def function_name():
+	'''
+	'''
+
+def HelloWorld():
+	print('Helloworld')
+
+HelloWorld() 		                	#Helloworld
+
+def HelloWorld():
+	print('Hello World, this is my first function.')
+
+len()
+range()
+print()
+input()
+
+det add(x,y):   	            	  # x and y are parameters
+return x+y      	            	  # return keyword
+
+add(5,4)
+				  # 9
+type(HelloWorld())  	         	  #Hello World, this is my first function.
+				                          #NoneType
+type(None)		                	  #NoneType
